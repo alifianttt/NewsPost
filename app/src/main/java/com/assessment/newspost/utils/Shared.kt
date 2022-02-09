1eb2f87636e569435a7f9cf5dc1fe76185ee38a4
Muhammad Yoga Alifianto <muhammad.alifianto@students.amikom.ac.id>
2022-02-09 09:19:47 +0700

    add reusable function

--- app/src/main/java/com/assessment/newspost/utils/Shared.kt
+++ app/src/main/java/com/assessment/newspost/utils/Shared.kt
@@ -3,6 +3,8 @@ package com.assessment.newspost.utils
 import android.app.Activity
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.load.model.LazyHeaders
 
 fun Activity.toasShort(message: String){
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
@@ -10,4 +12,12 @@ fun Activity.toasShort(message: String){
 
 fun View.setVisible(visible: Boolean){
     visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+fun getUrl(url: String): GlideUrl {
+    return GlideUrl(
+        url, LazyHeaders.Builder()
+            .addHeader("User-Agent", Constants.CONFIG.USER_AGENT)
+            .build()
+    )
 }
